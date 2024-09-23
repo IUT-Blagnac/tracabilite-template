12282ab6680752ac642650db32d2d7df8a8636f2
NariaReynhard <nariareynhard@gmail.com>
2024-09-23 16:49:35 +0200

    Implémentation hello world

---
+++ my-app/src/main/java/com/example/myapp/App.java
@@ -0,0 +1,21 @@
+package com.example.myapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    /**
+     * Simple function to return "Hello World !" when no parameter
+     * @return "Hello World !"
+     */
+    static String hello() {
+        return "Hello World !";
+    }
+}
