2b70a78484cbc4e4ba0bc45199ddd1490929c26d
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-09-23 16:44:50 +0200

    Update App.java
    ajout d'une javadoc

--- trace/src/main/java/com/iutblagnac/r303trace/App.java
+++ trace/src/main/java/com/iutblagnac/r303trace/App.java
@@ -14,7 +14,11 @@ public class App
     public static String hello() {
         return  "Hello World!" ;
     }
-
+    /**
+     * return ce qui est renseigné en paramètre d'entrée. ouah
+     * @param param
+     * @return param 
+     */
     public static String hello(String param) {
         return  param ;
     }
