434cce96c8a27f531d55d2ad372620f1f3d34341
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-09-23 17:01:35 +0200

    modif

--- trace/src/main/java/com/iutblagnac/r303trace/App.java
+++ trace/src/main/java/com/iutblagnac/r303trace/App.java
@@ -11,10 +11,19 @@ public class App
         System.out.println(hello());
     }
 
+    /**
+     * méthode qui retourne hello world si pas de paramètres
+     * @return Hello world
+     */
     public static String hello() {
         return  "Hello World!" ;
     }
 
+    /**
+     * méthode qui retourne le paramètre entré
+     * @param param un string a afficher
+     * @return le string que l'on veut afficher
+     */
     public static String hello(String param) {
         return  param ;
     }
