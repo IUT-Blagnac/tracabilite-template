31ba4e19b84547a838a85185e7e5345defbb1137
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-09-23 16:45:40 +0200

    Modification mineure

--- trace/src/main/java/com/iutblagnac/r303trace/App.java
+++ trace/src/main/java/com/iutblagnac/r303trace/App.java
@@ -5,13 +5,18 @@ package com.iutblagnac.r303trace;
  *
  */
 //@author: Théo
+//
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println(hello());
     }
-
+    /*
+     * @return "Hello World"
+     * 
+     */
+     
     public static String hello() {
         return  "Hello World!" ;
     }
