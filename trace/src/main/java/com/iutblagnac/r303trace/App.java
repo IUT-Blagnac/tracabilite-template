81d90a1fd3a8fb597b62e4bf78bf87a3dc9c311b
IUT BLAGNAC <noreply@univ-tlse2.fr>
2024-09-23 17:00:13 +0200

    Update App.java
    #2

--- trace/src/main/java/com/iutblagnac/r303trace/App.java
+++ trace/src/main/java/com/iutblagnac/r303trace/App.java
@@ -2,21 +2,16 @@ package com.iutblagnac.r303trace;
 
 /**
  * Hello world!
- *
+ * @author    Théo
+ * 
  */
-//@author:  Théo
-//
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println(hello());
     }
-    /*
-     * @return "Hello World"
-     * 
-     */
-     
+
     public static String hello() {
         return  "Hello World!" ;
     }
