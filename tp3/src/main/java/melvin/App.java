c34603954d0266d625007a4ca478393179bf8530
Melvin <melvin.bouyssou@gmail.com>
2024-09-23 16:31:47 +0200

    hello world test

---
+++ tp3/src/main/java/melvin/App.java
@@ -0,0 +1,13 @@
+package melvin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ tp3/src/test/java/melvin/AppTest.java
@@ -0,0 +1,20 @@
+package melvin;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Test si le App.main() retourne "Hello World!"
+     */
+    @Test
+    public void testHelloWorld(){
+        assertEquals("Hello World!", App.main());
+    }
+}
